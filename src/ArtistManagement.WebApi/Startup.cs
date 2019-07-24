4a5df81937ec713ef3341df14911182e55e15074
Ruddy Cahyadi <ruddycahyadi@gmail.com>
2019-07-24 16:41:39 +0700

    custom ui topbar

--- src/ArtistManagement.WebApi/Startup.cs
+++ src/ArtistManagement.WebApi/Startup.cs
@@ -50,6 +50,8 @@ namespace ArtistManagement.WebApi
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApiVersionDescriptionProvider provider)
         {
+            app.UseStaticFiles();
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
